f7834e028204de5148d4fecf69608df6d88a1d1d
Gabriel <gabrielalmeida@mac.myfiosgateway.com>
2025-01-22 23:57:10 -0500

    added you are my sunshine Nicole

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@ int main (void){
        
        puts ("Nicole+Gabriel = <3");
        puts ("As long as we are together, everything will be a-okay!");
+       puts ("You are my sunshine, my only sunshine :)");
 
         return 0;
 }
